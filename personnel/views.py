92549b20468d3e5a4a5743961d410f735fec2923
aliaydogdu105 <aliaydogdu105@gmail.com>
2023-04-29 23:11:53 +0300

    feat: add personnel-department_view

--- personnel/views.py
+++ personnel/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Department, Personnel
+from .serializers import DepartmentSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsStafforReadOnly
 
-# Create your views here.
+class DepartmentView(generics.ListCreateAPIView):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+    permission_classes = [IsAuthenticated, IsStafforReadOnly]
+
